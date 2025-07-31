314da9552eff3d4c56963a54c7b5db29a6cd9088
Osouzxz <143731722+Osouzxz@users.noreply.github.com>
2025-07-31 19:59:10 -0300

    ok

---
+++ SenhaSegura-main/main.js
@@ -0,0 +1,49 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';  
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha >1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].chacked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].chacked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].chacked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].chacked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
+    
+}
